dd9c9fda4f2545395e6d2ff1614b305723bf9a80
Ira <103947302+Ira8694733@users.noreply.github.com>
2023-10-30 20:38:56 +0200

    created lesson_1

---
+++ lesson_1.js
@@ -0,0 +1,36 @@
+//1) Create a variable with the name of our planet. 
+let ourPlanetName = "Earth";
+//2) Create a variable to store the name of the current site visitor.
+let visitorName = "Miranda";
+//3) Create "var" variable named myAge and set its value equal to 30
+var myAge = 30;
+//4) Create "let" variable named myName and set its value equal to any name
+let myName = "Ira";
+//5) Create "const" variable named myBool and set its value equal to false
+const myBool = false;
+//6) Display the sum of 15 + 30, using two variables: x and y.
+let x = 15, y = 30;
+console.log(x+y);
+//7) Create variable named myUndefined and set its value equal to undefined
+let myUndefined = undefined;
+//8) Create variable named dividedNumber and set its value equal myAge divided by 2
+// * NOTE: you should use myAge variable here
+let myNumber = 2;
+let dividedNumber = myAge/myNumber;
+console.log(dividedNumber);
+//9) Create variable named multipleNumber and set its value equal myAge multiply by 2
+let multipledNumber = myAge*myNumber;
+console.log(multipledNumber);
+//10) Create variable named mySentence and set its value to be myName, then goes space and word is, then space and myAge
+//NOTE: Result should be something like "Michael is 30"
+let mySentense = myName + " is " + myAge;
+console.log(mySentense);
+//11) Create variable named myUndefined2 and set its value equal to undefined without "undefined" word usage
+let boolean = undefined;
+let myUndefined2 = boolean;
+//12) Declare two variables: admin and name. Assign the value "Bill Gates" to the variable name.Copy the value from the variable name to admin.
+//Print the value of the admin variable.
+let admin, name; 
+name = "Bill Gates"; 
+admin = name; 
+console.log(admin);
