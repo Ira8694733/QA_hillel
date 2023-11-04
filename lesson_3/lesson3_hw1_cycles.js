b4dfaaa34a4a4a3228290ed41dc3c8e93d928d3d
Ira <103947302+Ira8694733@users.noreply.github.com>
2023-11-04 22:47:49 +0200

    lesson3_hw_1cycles.js is done

--- lesson_3/lesson3_hw1_cycles.js
+++ lesson_3/lesson3_hw1_cycles.js
@@ -8,10 +8,40 @@ for (let i = 0; i < 8; i++) {
 
 */
 
+let i = 0;
+while (i < 8) {
+  console.log(`число - ` + i);
+  i++;
+}
 
 /*Task - 2
   Implement algorithm which will calculate sum of numbers from 0 to 50 (included) not divisible by 3.
   Use 3 different cycles: for | while | do..while */
+
+let sum = 0;
+for (let i = 0; i <= 50; i++) { // ініціалізує змінну перемінної sum до 0. Потім він перебирає числа від 0 до 50
+  if (i % 3 !== 0) { //  і для кожного числа перевіряє, чи воно не ділиться на 3, використовуючи оператор  %. 
+    sum += i; // Якщо воно не ділиться, воно додає число до суми. 
+  }
+}
+console.log("The sum of numbers from 0 to 50 (not divisible by 3) is:", sum)
+
+while (sum < 50) {
+  if (i % 3 !== 0) { 
+    sum += i; 
+  }
+  i++;
+}
+console.log("The sum of numbers from 0 to 50 (not divisible by 3) is:", sum)
+
+
+  do {
+    console.log("The sum of numbers from 0 to 50 (not divisible by 3) is:", sum)
+    if (sum % 3 !== 0) {
+      sum += sum;
+    }
+    sum++;
+  } while (sum <=50);
 
 /* Task - 3
   Implement algorithm which will
@@ -21,6 +51,13 @@ for (let i = 0; i < 8; i++) {
 
 let firstNumber = -100;
 let secondNumber = 250;
+let sumOfNumbers = 0;
+
+for (let i = firstNumber; i <= secondNumber; i++) // Він продовжується, доки i <= secondNumber, збільшуючи i на 1 у кожній ітерації.
+ {
+  sumOfNumbers += i; // Усередині циклу ми додаємо i до суми в кожній ітерації.
+}
+console.log(sumOfNumbers);
 
 /* Task - 4
   Make a timer that will count from 10 to 0
@@ -31,8 +68,18 @@ let secondNumber = 250;
   8 seconds left 
 */
 
+  for (let i = 10; i >= 0; i--) {
+       console.log("Time's up!",  +i);
+    }
+
 /* Task - 5
   Create program to display the following sequence: 7 14 21 28 35 42 49 */
+
+  for (let i = 7; i <= 49; i--) {
+    i+=7
+    console.log("Time's up!",  i);
+  }
+
 
 /* Task - 6
   implement algorithm which calculates factorial for 
